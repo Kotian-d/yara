1621ddd4b2656076a8da433dc20829e2cc00115e
Kotian-d <kotiand82@gmail.com>
2025-08-05 16:42:46 +0530

    testing_jwt_expiry3

--- app/services/jwthelpers.js
+++ app/services/jwthelpers.js
@@ -19,7 +19,7 @@ export const generateRefreshToken = async (userId) => {
   const jwt = await new SignJWT({ userId })
     .setProtectedHeader({ alg: "HS256" }) // Algorithm for signing
     .setIssuedAt() // Set issued at timestamp//Set audienece
-    .setExpirationTime("1y") // Token expires in 1 hour
+    .setExpirationTime("1m") // Token expires in 1 hour
     .sign(encodedSecret); // Sign with the secret key
 
   return jwt;
