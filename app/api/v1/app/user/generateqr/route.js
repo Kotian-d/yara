de249be13e2fdd9062d1cc9aa3af31054de2ae22
Shreyas-2004-ai <shreyasssanil62@gmail.com>
2025-10-10 12:24:38 +0530

    payment_gateway_update

--- app/api/v1/app/user/generateqr/route.js
+++ app/api/v1/app/user/generateqr/route.js
@@ -1,24 +1,40 @@
 import { NextResponse } from "next/server";
-import CreateOrderAPI from "@/app/services/tezgateway";
+import CreateOrderAPI from "@/app/integration/tezgateway";
+
+function generateOrderNumber() {
+  // The smallest 12-digit number is 10^11
+  const min = 100000000000; 
+  // The largest 12-digit number is 10^12 - 1
+  const max = 999999999999; 
+
+  // Generate a random number within the defined range (inclusive)
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  return randomNumber;
+}
 
 export async function POST(request) {
-    const { amount } = await request.json();
+  const { amount } = await request.json();
+
+  if(amount == null || parseInt(amount) <= 0 || parseInt(amount) > 10000) return NextResponse.json({ status: "error", message: "Please enter the amount between 1 to 10000" }, { status: 400 });
+
+  const orderid = generateOrderNumber();
   const api = new CreateOrderAPI("https://tezgateway.com/api/create-order");
   try {
-    await api.createOrder(
+
+    const order = await api.createOrder(
       "9739283261",
       "2e4de2cdf3abaf525644f88de06e4436",
       amount,
-      "8787772321800",
+      orderid.toString(),
       "https://https://yara-one.vercel.app/callback/8787772321800",
       "testremark",
       "testremark2"
-    );
-
-    console.log("Order created:", order);
+    );   
+  
     return NextResponse.json({ order });
   } catch (error) {
-    console.error("Order creation failed:", error);
-    return NextResponse.json({ error: error.message });
+    console.log("Order creation failed:", error.message);
+    return NextResponse.json({ status: "error", message: error.message }, { status: 500 });
   }
 }
+
